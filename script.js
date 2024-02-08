bc2dfd266bcb291fd707828ea3f8ab2a78534e73
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-02-08 15:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const table=document.getElementById('sampleTable');
+	const newRow=table.insertRow(0);
+	newRow.insertAdjacentHTML('beforeend', '<td>New Cell1</td><td>New Cell2</td>')
 }
